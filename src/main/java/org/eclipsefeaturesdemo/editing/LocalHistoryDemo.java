35ed10600e28d6a9df539f28b889b52eee75934d
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2026-07-06 22:23:19 +0530

    Create LocalHistoryDemo.java

---
+++ src/main/java/org/eclipsefeaturesdemo/editing/LocalHistoryDemo.java
@@ -0,0 +1,13 @@
+package org.eclipsefeaturesdemo.editing;
+
+public class LocalHistoryDemo {
+
+    public static void main(String[] args) {
+        System.out.println(greet("Eclipse Developer"));
+    }
+
+    private static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
+
